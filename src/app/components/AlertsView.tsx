f3889b0afac5d728349a3e82e0c88d184896e3e7
ta12668 <ta12668@gmail.com>
2026-05-28 22:10:17 +0700

    fix(ui): remove MacroMonitor widgets (USD/THB, LME Copper, Trade War Alert)

--- src/app/components/AlertsView.tsx
+++ src/app/components/AlertsView.tsx
@@ -3,13 +3,11 @@
 import AlertHero from "./AlertHero";
 import AlertTable from "./AlertTable";
 import KPICards from "./KPICards";
-import MacroMonitor from "./MacroMonitor";
 
 export default function AlertsView({ approvedPlans = [] }: { approvedPlans?: any[] }) {
   return (
     <div className="space-y-4">
       <AlertHero />
-      <MacroMonitor />
       <KPICards />
       <AlertTable approvedPlans={approvedPlans} />
     </div>
